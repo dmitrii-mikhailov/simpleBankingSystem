438dd4dd1c136507ef5dafc2399c3a776a12b705
Dmitrii Mikhailov <dmitriy.mikhailoff@gmail.com>
2022-12-15 23:35:34 +0300

    Added jdbc interaction

--- banking/Main.java
+++ banking/Main.java
@@ -1,9 +1,16 @@
 package banking;
 
+import banking.database.AccountDB;
 import banking.menu.Menu;
 
 public class Main {
+    private static String[] savedArgs;
+    public static String[] getArgs() {
+        return savedArgs;
+    }
     public static void main(String[] args) {
+        savedArgs = args;
+        AccountDB.createDataBase();
         Menu menu = new Menu();
         menu.startMenu();
     }
