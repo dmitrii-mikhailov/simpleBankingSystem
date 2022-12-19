2ec47fb67b144d241af14b8ed6a47f9736ba594a
Dmitrii Mikhailov <dmitriy.mikhailoff@gmail.com>
2022-12-19 21:28:53 +0300

    Added functionality: Now your menu looks like this:
    1. Balance
    2. Add income
    3. Do transfer
    4. Close account
    5. Log out
    0. Exit
    If the user asks for Balance,  the balance of the account is retrieved from the database and output it into the console.
    
    Add income item allows us to deposit money to the account.
    
    Do transfer item allows transferring money to another account. Following errors are handled:
    
    If the user tries to transfer more money than he/she has, program outputs: Not enough money!
    If the user tries to transfer money to the same account, program outputs the following message: You can't transfer money to the same account!
    If the receiver's card number doesn’t pass the Luhn algorithm, program outputs: Probably you made a mistake in the card number. Please try again!
    If the receiver's card number doesn’t exist, program outputs: Such a card does not exist.
    If there is no error, program asks the user how much money they want to transfer and makes the transaction.
    If the user chooses the Close an account item, program deletes that account from the database.

--- banking/database/AccountDB.java
+++ banking/database/AccountDB.java
@@ -11,10 +11,10 @@ public class AccountDB {
     public static int getNumberOfObjInArr() {
         SQLiteDataSource dataSource = createDataSource();
         int numberOfCards = -1;
-
+        String selectCount = "SELECT COUNT (id) AS total FROM CARD";
         try (Connection con = dataSource.getConnection()) {
-            try (Statement statement = con.createStatement()) {
-                try (ResultSet cards = statement.executeQuery("SELECT COUNT (id) AS total FROM CARD")) {
+            try (PreparedStatement statement = con.prepareStatement(selectCount)) {
+                try (ResultSet cards = statement.executeQuery()) {
                     numberOfCards = cards.getInt("total");
                 }
             } catch (SQLException e) {
@@ -32,18 +32,21 @@ public class AccountDB {
 
     public static void addAccount(Account account) {
         SQLiteDataSource dataSource = createDataSource();
-
+        String selectAccountSQL = "SELECT * FROM CARD ORDER BY id DESC LIMIT 1";
+        String insertAccountSQL = "INSERT INTO CARD VALUES (?, ?, ?, ?)";
         try (Connection con = dataSource.getConnection()) {
-            try (Statement statement = con.createStatement()) {
+            try (PreparedStatement selectAccount = con.prepareStatement(selectAccountSQL);
+                 PreparedStatement insertAccount = con.prepareStatement(insertAccountSQL)) {
                 int id;
-
-                try (ResultSet cards = statement.executeQuery("SELECT * FROM CARD ORDER BY id DESC LIMIT 1")) {
+                try (ResultSet cards = selectAccount.executeQuery()) {
                     boolean rowsExists = cards.isBeforeFirst();
                     id = rowsExists ? cards.getInt("id") + 1 : 1;
                 }
-
-                int i = statement.executeUpdate("INSERT INTO CARD VALUES " +
-                        "("+id+", '"+account.getCardNumber()+"', '"+account.getPin()+"', "+(int) account.getBalance()+")");
+                insertAccount.setInt(1, id);
+                insertAccount.setString(2, account.getCardNumber());
+                insertAccount.setString(3, account.getPin());
+                insertAccount.setInt(4, (int) account.getBalance());
+                insertAccount.executeUpdate();
 
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -55,11 +58,12 @@ public class AccountDB {
 
     public static Account getAccount(String cardNumber) {
         SQLiteDataSource dataSource = createDataSource();
+        String selectAccount = "SELECT * FROM CARD WHERE number = ?";
 
         try (Connection con = dataSource.getConnection()) {
-            try (Statement statement = con.createStatement()) {
-                try (ResultSet cards = statement.executeQuery("SELECT * FROM CARD " +
-                                                                 "WHERE number = '" + cardNumber + "'")) {
+            try (PreparedStatement statement = con.prepareStatement(selectAccount)) {
+                statement.setString(1, cardNumber);
+                try (ResultSet cards = statement.executeQuery()) {
                     boolean rowsExists = cards.isBeforeFirst();
                     if (rowsExists) {
                         return new Account(cards.getString("number"), cards.getString("pin"), cards.getInt("balance"));
@@ -83,15 +87,110 @@ public class AccountDB {
 
     public static void createDataBase() {
         SQLiteDataSource dataSource = createDataSource();
+        String createDB = "CREATE TABLE IF NOT EXISTS card(" +
+                "id INTEGER," +
+                "number TEXT," +
+                "pin TEXT," +
+                "balance INTEGER DEFAULT 0)";
         try (Connection con = dataSource.getConnection()) {
-            try (Statement statement = con.createStatement()) {
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS card(" +
-                        "id INTEGER," +
-                        "number TEXT," +
-                        "pin TEXT," +
-                        "balance INTEGER DEFAULT 0)");
+            try (PreparedStatement statement = con.prepareStatement(createDB)) {
+                statement.executeUpdate();
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateBalance(long income, String cardNumber) {
+        SQLiteDataSource dataSource = createDataSource();
+        String updateAccount = "UPDATE card " +
+                "SET balance = balance + ? " +
+                "WHERE number = ?";
+
+        try (Connection con = dataSource.getConnection()) {
+            con.setAutoCommit(false);
+            try (PreparedStatement statement = con.prepareStatement(updateAccount)) {
+                statement.setLong(1, income);
+                statement.setString(2, cardNumber);
+                statement.executeUpdate();
+                con.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                con.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static long retrieveBalance(String cardNumber) {
+        SQLiteDataSource dataSource = createDataSource();
+        String selectAccount = "SELECT * FROM CARD WHERE number = ?";
+
+        try (Connection con = dataSource.getConnection()) {
+            try (PreparedStatement statement = con.prepareStatement(selectAccount)) {
+                statement.setString(1, cardNumber);
+                try (ResultSet cards = statement.executeQuery()) {
+                    boolean rowsExists = cards.isBeforeFirst();
+                    if (rowsExists) {
+                        return cards.getLong("balance");
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Error occurred while retrieving balance!");
+    }
+
+    public static void transferMoney(String cardNumberFrom, String cardNumberTo, long moneyToTransfer) {
+        SQLiteDataSource dataSource = createDataSource();
+        String subtractMoneySQL = "UPDATE card " +
+                "SET balance = balance - ? " +
+                "WHERE number = ?";
+        String addMoneySQL = "UPDATE card " +
+                "SET balance = balance + ? " +
+                "WHERE number = ?";
+
+        try (Connection con = dataSource.getConnection()) {
+            con.setAutoCommit(false);
+            try (PreparedStatement subtractMoney = con.prepareStatement(subtractMoneySQL);
+                 PreparedStatement addMoney = con.prepareStatement(addMoneySQL)) {
+                subtractMoney.setLong(1, moneyToTransfer);
+                subtractMoney.setString(2, cardNumberFrom);
+                subtractMoney.executeUpdate();
+
+                addMoney.setLong(1, moneyToTransfer);
+                addMoney.setString(2, cardNumberTo);
+                addMoney.executeUpdate();
+                con.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                con.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteAccount(String cardNumber) {
+        SQLiteDataSource dataSource = createDataSource();
+        String deleteAccount = "DELETE from card " +
+                "WHERE number = ?";
+
+        try (Connection con = dataSource.getConnection()) {
+            con.setAutoCommit(false);
+            try (PreparedStatement statement = con.prepareStatement(deleteAccount)) {
+                statement.setString(1, cardNumber);
+                statement.executeUpdate();
+                con.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                con.rollback();
             }
         } catch (SQLException e) {
             e.printStackTrace();
--- banking/menu/Menu.java
+++ banking/menu/Menu.java
@@ -16,7 +16,10 @@ public class Menu {
     private void showAccountMenu() {
         System.out.println("""
                 1. Balance
-                2. Log out
+                2. Add income
+                3. Do transfer
+                4. Close account
+                5. Log out
                 0. Exit""");
     }
 
@@ -89,6 +92,16 @@ public class Menu {
         return null;
     }
 
+    private Account findAccount(String cardNumber) {
+        Account acc = AccountDB.getAccount(cardNumber);
+        if (Objects.nonNull(acc)) {
+            if (acc.getCardNumber().equals(cardNumber)) {
+                return acc;
+            }
+        }
+        return null;
+    }
+
     private boolean accountMenu(Account account) {
         boolean isWorking = true;
         Scanner scanner = new Scanner(System.in);
@@ -99,9 +112,18 @@ public class Menu {
 
             switch (input) {
                 case "1":
-                    System.out.println("\nBalance: " + account.getBalance() + "\n");
+                    System.out.println("\nBalance: " + AccountDB.retrieveBalance(account.getCardNumber()) + "\n");
                     break;
                 case "2":
+                    addIncome(account.getCardNumber());
+                    break;
+                case "3":
+                    transferTheMoney(account);
+                    break;
+                case "4":
+                    isWorking = deleteTheAccount(account);
+                    break;
+                case "5":
                     isWorking = false;
                     System.out.println("\nYou have successfully logged out!\n");
                     break;
@@ -112,5 +134,76 @@ public class Menu {
             }
         }
         return true;
+    }
+
+    private void addIncome(String cardNumber) {
+        Scanner scanner = new Scanner(System.in);
+        long income;
+        System.out.println("\nEnter income:");
+        income = scanner.nextLong();
+        AccountDB.updateBalance(income, cardNumber);
+        System.out.println("Income was added!\n");
+    }
+
+    static boolean checkLuhn(String cardNumber)
+    {
+        int nDigits = cardNumber.length();
+        int nSum = 0;
+        boolean isSecond = false;
+        for (int i = nDigits - 1; i >= 0; i--)
+        {
+            int d = cardNumber.charAt(i) - '0';
+            if (isSecond) {
+                d = d * 2;
+            }
+            nSum += d / 10;
+            nSum += d % 10;
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
+    }
+
+    private void transferTheMoney(Account account) {
+        Scanner scanner = new Scanner(System.in);
+        String cardToTransfer;
+        System.out.println("\nTransfer");
+        System.out.println("Enter card number:");
+        cardToTransfer = scanner.nextLine();
+        if (account.getCardNumber().equals(cardToTransfer)) {
+            System.out.println("You can't transfer money to the same account!\n");
+            return;
+        }
+        if (!checkLuhn(cardToTransfer)) {
+            System.out.println("Probably you made a mistake in the card number. Please try again!\n");
+            return;
+        }
+        Account accountToTransfer = findAccount(cardToTransfer);
+        if (Objects.isNull(accountToTransfer)) {
+            System.out.println("Such a card does not exist.\n");
+            return;
+        }
+        System.out.println("Enter how much money you want to transfer:");
+        long moneyToTransfer = scanner.nextLong();
+        if (AccountDB.retrieveBalance(account.getCardNumber()) < moneyToTransfer) {
+            System.out.println("Not enough money!\n");
+            return;
+        }
+        try {
+            AccountDB.transferMoney(account.getCardNumber(), accountToTransfer.getCardNumber(), moneyToTransfer);
+            System.out.println("Success!\n");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private boolean deleteTheAccount(Account account) {
+        try {
+            AccountDB.deleteAccount(account.getCardNumber());
+            System.out.println("\nThe account has been closed!\n");
+            return false;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
     }
 }
